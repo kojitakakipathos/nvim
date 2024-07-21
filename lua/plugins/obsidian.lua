afc0380a0c65645f20506e6a675e602f471402dd
Koji Takaki <koji.takaki@pathoslogos.co.jp>
2024-07-21 15:31:24 +0900

    [add] first commit

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,10 @@
+return {
+
+  -- obsidianと日本語で検索する用のプルグイン
+  "delphinus/obsidian-kensaku.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "lambdalisue/vim-kensaku",
+    "vim-denops/denops.vim",
+  },
+}
